cdb7d9a9ec181e48e556c90912dfb83fa17fe23a
Kim Wook <kquan821@gmail.com>
2022-09-16 00:32:39 +0700

    update

--- App.js
+++ App.js
@@ -1,27 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-import Register from './src/pages/Register';
-import Login from './src/pages/Login';
-import FormLogin from './src/pages/FormLogin';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Login from "./src/login/login";
 export default function App() {
   return (
     <View style={styles.container}>
-      <FormLogin />
-      <Register />
       <Login />
     </View>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
   },
-
 });
